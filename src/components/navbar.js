9feeb99e6c937492f1a7dc3cde961ad3946ed5f5
Allen Welch <allenjwelch@gmail.com>
2018-06-20 23:00:35 -0400

    updated navbar background

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,18 +6,22 @@ const styles = {
   },
   guide: {
     fontSize: "24px"
+  },
+  navbar: {
+    background: "#050505",
+    color: "#ffffff", 
   }
 }
 
 const Nav = props => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <nav className="navbar navbar-expand-lg" style={styles.navbar}>
     <h1 className="navbar-brand" style={styles.logo} href="#">Click-It-To-Win-It!</h1>
 
-    <div className="guide mx-auto" style={styles.guide}>Click an Image to Begin!</div>
+    {/* <div className="guide mx-auto" style={styles.guide}>Click an Image to Begin!</div> */}
     
-    <div className="scoreboard">
-      <h4>User Score: <span className="navbar-text" id="userScore">{props.userScore}</span></h4>
-      <h4>Top Score: <span className="navbar-text" id="topScore">{props.topScore}</span></h4>
+    <div className="scoreboard ml-auto d-flex">
+      <h4>User Score: <span className="p-2" id="userScore">{props.userScore}</span></h4>
+      <h4>Top Score: <span className="p-2" id="topScore">{props.topScore}</span></h4>
     </div>
   </nav>
 ); 
